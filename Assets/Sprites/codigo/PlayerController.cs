e68a2b7e60c9b810991cf7974ff7d9d86d539e84
aa <a@a>
2026-09-16 13:59:07 +0200

    gfrge

---
+++ Assets/Sprites/codigo/PlayerController.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    //int para guardar la vida maxima
+    [SerializeField]private int _maxHealth = 100;
+    [SerializeField]private float _movementSpeed = 4.5f;
+    [SerializeField]private float _forceJump = 10;
+    
+    /*float decimales = 5,4f;
+
+    bool boleana = true;
+    
+    string texto = "asdasdadasd";*/
+
+private Rigidbody2D _rigidbody2D;
+
+private InputAction _moveAction;
+
+private InputAction _JumpAction;
+
+private Vector2 _moveInput;
+
+[SerializeField] private Transform _groundSensor;
+[SerializeField] private float _sensorSize = 1;
+[SerializeField] private LayerMask _groundLayer;
+
+
+
+    void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+
+        _moveAction = InputSystem.actions["Move"];
+
+        _JumpAction = InputSystem.actions["Jump"];
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _moveInput = _moveAction.ReadValue<Vector2>();
+
+        if(_moveInput.x < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if(_moveInput.x > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+
+        if(_JumpAction.WasPressedThisFrame() && IsGrounded())
+        {
+            Jump();
+        }
+    }
+
+    void FixedUpdate()
+    {  
+        _rigidbody2D.linearVelocity = new Vector2(_moveInput.x * _movementSpeed, _rigidbody2D.linearVelocity.y);
+    }
+
+    void Jump()
+    {
+        _rigidbody2D.AddForce(Vector2.up * _forceJump, ForceMode2D.Impulse);
+    }
+
+    bool IsGrounded()
+    {
+        Collider2D[] colliders2D = Physics2D.OverlapCircleAll(_groundSensor.position, _sensorSize);
+
+        foreach (Collider2D item in colliders2D)
+        {
+            if(item.gameObject.layer == 6)
+            {
+            return true;
+            }
+
+        }
+        return false;        
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_groundSensor.position, _sensorSize);
+    }
+}
